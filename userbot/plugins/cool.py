fd045f23ce2044e84bfe888a7fe439f174b3462c
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-24 19:39:01 +0530

    Update cool.py

--- userbot/plugins/cool.py
+++ userbot/plugins/cool.py
@@ -7,7 +7,7 @@ import asyncio
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.3
+    animation_interval = 0.4
     animation_ttl = range(0, 16)
     input_str = event.pattern_match.group(1)
     if input_str == "cool":
@@ -22,4 +22,4 @@ async def _(event):
         ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 4])
+            await event.edit(animation_chars[i % 6])
