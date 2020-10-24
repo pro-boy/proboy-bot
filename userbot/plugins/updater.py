369c294c65d7f8e3e3e03c1eebf34cc3dccab6d5
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-24 07:00:09 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -27,7 +27,7 @@ NEW_UP_DATE_FOUND = (
     "Updating And Restarting..."
 )
 REPO_REMOTE_NAME = "temponame"
-IFFUCI_ACTIVE_BRANCH_NAME = "master"
+IFFUCI_ACTIVE_BRANCH_NAME = "main"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
 NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
 HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
