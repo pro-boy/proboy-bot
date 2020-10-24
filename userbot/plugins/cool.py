b018a19fdb6bad64fd5ad5ff64d8c2ce91c22caf
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-24 19:32:24 +0530

    Update cool.py

--- userbot/plugins/cool.py
+++ userbot/plugins/cool.py
@@ -3,7 +3,7 @@ from telethon import events
 import asyncio
 
 
-@borg.on(events.NewMessage(pattern=r"\.cool(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
