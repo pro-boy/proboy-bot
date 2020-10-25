518749d76580434e966c8e12c0946f0067b39f00
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-26 02:01:52 +0530

    Update adult.py

--- userbot/plugins/adult.py
+++ userbot/plugins/adult.py
@@ -6,19 +6,19 @@ from userbot.utils import admin_cmd
 from userbot import bot
 
 borg.on(admin_cmd(pattern='random ?(.*) '))
-@borg.on(events.NewMessage(pattern='.random (.*)'))
+@borg.on(events.NewMessage(pattern='.^random (.*)'))
 async def getmusic(so):
     if so.fwd_from:
         return
     song = so.pattern_match.group(1)
     chat = "@Epornerbot"
-    link = "/random"
+    msg = "/random"
     await so.edit("searching ur song Boss🔍")
     async with bot.conversation(chat) as conv:
           await asyncio.sleep(2)
           await so.edit("select the song ßoss😅😅")
           try:
-              msg = await conv.send_message(link)
+              msg = await conv.send_message(msg)
               response = await conv.get_response()
               respond = await conv.get_response()
               """ - don't spam notif - """
@@ -27,8 +27,8 @@ async def getmusic(so):
               await so.edit("```Please unblock @NeosMusicBot and try again```")
               return
           await so.edit("ur download is here😁")
-          await asyncio.sleep(1)
-          await bot.send_file(so.chat_id, respond)
+          await asyncio.sleep()
+          await bot.send_img(so.chat_id, respond)
     await so.client.delete_messages(conv.chat_id,
                                        [msg.id, response.id, respond.id])
     await so.delete()
