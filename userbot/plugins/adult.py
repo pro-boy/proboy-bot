204664a9b4fc00a041f3cae64840c19fd11629fb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-26 10:08:37 +0530

    Update adult.py

--- userbot/plugins/adult.py
+++ userbot/plugins/adult.py
@@ -26,13 +26,8 @@ async def _(event):
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
               await borg.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.edit("```Please unblock @sangmatainfo_bot and try again```")
-              return
-          if response.text.startswith("Forward"):
-              await event.edit("```can you kindly disable your forward privacy settings for good?```")
-          else: 
+             
+          
               await borg.send_file(event.chat_id, response.message.media)
     await event.client.delete_messages(conv.chat_id,
                                        [msg.id, response.id, respond.id])
