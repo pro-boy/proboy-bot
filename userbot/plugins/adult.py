eee5b411cab47307a66d19fde263467c344119b3
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-26 23:08:28 +0530

    Update adult.py

--- userbot/plugins/adult.py
+++ userbot/plugins/adult.py
@@ -55,9 +55,11 @@ async def _(event):
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
 
               await bot.forward_messages(chat, reply_message)
+              response = await conv.get_response()
+              respond = await conv.get_response()
 
-              response = await response 
-
+            await bot.send_read_acknowledge(conv.chat_id)
+         
           except YouBlockedUserError: 
 
               await event.reply("```Please unblock @QuotLyBot and try again```")
