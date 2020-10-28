0f68fc25103ad9c7cc3d858cff6edc326894385f
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-29 01:06:33 +0530

    Update pm_logger.py

--- userbot/plugins/pm_logger.py
+++ userbot/plugins/pm_logger.py
@@ -53,11 +53,11 @@ async def log(log_text):
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def monito_p_m_s(event):
     sender = await event.get_sender()
-    if Config.NC_LOG_P_M_S and not sender.bot:
+    if Config.No_LOG_P_M_S and not sender.bot:
         chat = await event.get_chat()
         if chat.id not in NO_PM_LOG_USERS and chat.id != borg.uid:
             try:
-                e = await borg.get_entity(BOTLOG_CHATID)          
+                e = await borg.get_entity(int(BOTLOG_CHATID))          
                 fwd_message = await borg.forward_messages(
                     e,
                     event.message,
