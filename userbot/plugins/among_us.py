37f5f12cc008931a93e4844f9454e4f08f261a0e
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-31 14:24:03 +0530

    Update among_us.py

--- userbot/plugins/among_us.py
+++ userbot/plugins/among_us.py
@@ -18,7 +18,7 @@ async def _(event):
     name = event.pattern_match.group(2)
     cmd = event.pattern_match.group(1).lower()
     
-    text1 = await edit.edit("Hmm... Looks like Something is wrong here🤔🧐!!")
+    text1 = await event.edit("Hmm... Looks like Something is wrong here🤔🧐!!")
     await asyncio.sleep(2)
     await text1.delete()
     stcr1 = await event.client.send_file(
