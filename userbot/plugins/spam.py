b5438e865bae1d54860268a2767a9590b01cc4bb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-10-31 17:48:25 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -9,28 +9,6 @@ from asyncio import wait
 
 
 from userbot.events import register
-from telethon import events
-
-
-
-@borg.on(events.NewMessage(pattern=r"\.spam", outgoing=True))
-async def spammer(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        counter = int(message[6:8])
-        spam_message = str(e.text[8:])
-
-        await wait(
-            [e.respond(spam_message) for i in range(counter)]
-            )
-
-        await e.delete()
-        if LOGGER:
-            await e.client.send_message(
-                LOGGER_GROUP,
-                "#SPAM \n\n"
-                "Spam was executed successfully"
-                )
 
 @register(outgoing=True, pattern="^.tspam")
 async def tmeme(e):
@@ -101,4 +79,42 @@ async def spammer(e):
         await e.client.send_message(
             LOGGER_GROUP, "#DelaySPAM\n"
             "DelaySpam was executed successfully")
+        
             
+@register(outgoing=True, pattern="^.mspam (.*)")
+
+async def tiny_pic_spam(e):
+
+  sender = await e.get_sender() ; me = await e.client.get_me()
+
+  if  sender.id == me.id and  FULL_SUDO:
+
+       return await e.reply("`Sorry sudo users cant access this command..`")
+
+  try:
+
+       await e.delete()
+
+  except:
+
+    	pass
+    
+  try:
+
+    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
+
+    reply_message = await e.get_reply_message() 
+
+    if not reply_message or not e.reply_to_msg_id or not reply_message.media or not reply_message.media:
+
+       return await e.edit("```Reply to a pic/sticker/gif/video message```")
+
+    message = reply_message.media
+
+    for i in range(1, counter):
+
+        await e.client.send_file(e.chat_id, message)
+
+  except:      
+
+        return await e.reply(f"**Error**\nUsage `!mspam <count> reply to a sticker/gif/photo/video`")
