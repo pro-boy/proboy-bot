0963c9f411152008905c1b74c3464c33bdffded6
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 03:28:18 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -87,7 +87,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             ©DanishBot".format(plugin_name)
         try:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-            except BaseException:
+            except:
                 # https://github.com/Dark-Princ3/X-tra-Telegram/commit/275fd0ec26b284d042bf56de325472e088e6f364#diff-2b2df8998ff11b6c15893b2c8d5d6af3
                 with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
                     out_file.name = "{}.txt".format(plugin_name)
