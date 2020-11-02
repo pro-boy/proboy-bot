7290c4b71d213471dd14dd28082aca2625b9f68b
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 13:18:43 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -11,7 +11,7 @@ NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = os.environ.get(
 )
 EMOJI_TO_DISPLAY_IN_HELP = os.environ.get("EMOJI_TO_DISPLAY_IN_HELP", None)
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else @danish_00"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@danish_00"
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
