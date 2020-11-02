d70245c26461d511abd98b5bfe7ebd831b063869
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 07:50:57 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -26,84 +26,126 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 link_preview=False
             )
         await event.answer([result] if result else None)
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"helpme_next\((.+?)\)")
-    ))
+     @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(rb"helpme_next\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
-            buttons = paginate_help(
-                current_page_number + 1, CMD_LIST, "helpme")
-           
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(current_page_number + 1, CMD_LIST, "helpme")
+            # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Hey don't touch buttons!! Who tf give u permission for that🧐?"
+            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"helpme_prev\((.+?)\)")
-    ))
+    @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(rb"helpme_prev\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
             buttons = paginate_help(
-                current_page_number - 1,
-                CMD_LIST,  # pylint:disable=E0602
-                "helpme"
+                current_page_number - 1, CMD_LIST, "helpme"  # pylint:disable=E0602
             )
-         
+            # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Hey don't touch buttons!! Who tf give u permission for that🧐??"
+            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
+    async def on_plug_in_callback_query_handler(event):
+        timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
+        if os.path.exists("./userbot/secrets.txt"):
+            jsondata = json.load(open("./userbot/secrets.txt"))
+            try:
+                message = jsondata[f"{timestamp}"]
+                userid = message["userid"]
+                ids = [userid, bot.uid]
+                if event.query.user_id in ids:
+                    encrypted_tcxt = message["text"]
+                    reply_pop_up_alert = encrypted_tcxt
+                else:
+                    reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+            except KeyError:
+                reply_pop_up_alert = "This message no longer exists in bot server"
+        else:
+            reply_pop_up_alert = "This message no longer exists "
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"us_plugin_(.*)")
+        )
+    )
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:
+            plugin_name = event.data_match.group(1).decode("UTF-8")
+            help_string = ""
+            try:
+                for i in CMD_LIST[plugin_name]:
+                    help_string += i
+                    help_string += "\n"
+            except BaseException:
+                pass
+            if help_string == "":
+                reply_pop_up_alert = "{} is useless".format(plugin_name)
+            else:
+                reply_pop_up_alert = help_string
+            reply_pop_up_alert += (
+                "Use .unload {} to remove this plugin ©catuserbot".format(plugin_name)
+            )
+            try:
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            except BaseException:
+                # https://github.com/Dark-Princ3/X-tra-Telegram/commit/275fd0ec26b284d042bf56de325472e088e6f364#diff-2b2df8998ff11b6c15893b2c8d5d6af3
+                with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
+                    out_file.name = "{}.txt".format(plugin_name)
+                    await event.client.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption=plugin_name,
+                    )
+        else:
+            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
-            await event.edit("The menu has been closed..!")
-
-
-
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"us_plugin_(.*)")
-    ))
-    async def on_plug_in_callback_query_handler(event):
-        plugin_name = event.data_match.group(1).decode("UTF-8")
-        help_string = ""
-        try:
-            for i in CMD_LIST[plugin_name]:
-                help_string += i
-                help_string += "\n"
-        except:
-            pass
-        if help_string is "":
-            reply_pop_up_alert = "{} is useless".format(plugin_name)
+            await event.edit("menu closed")
         else:
-            reply_pop_up_alert = help_string
-        reply_pop_up_alert += "\n Use .restart or .unload {} to remove this plugin\n\
-            ©DanishBot".format(plugin_name)
-        try:
+            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        except: 
-            halps = "Do .help {} to get the list of commands.".format(plugin_name)
-            await event.answer(halps, cache_time=0, alert=True)
-    
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
+    async def on_plug_in_callback_query_handler(event):
+        statstext = await catalive()
+        reply_pop_up_alert = statstext
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
-    multi = Config.EMOJI_TO_DISPLAY_IN_HELP
-    helpable_plugins = []
-    for p in loaded_plugins:
-        if not p.startswith("_"):
-            helpable_plugins.append(p)
+    helpable_plugins = [p for p in loaded_plugins if not p.startswith("_")]
     helpable_plugins = sorted(helpable_plugins)
-    modules = [custom.Button.inline(
-        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
-        data="us_plugin_{}".format(x))
-        for x in helpable_plugins]
+    modules = [
+        custom.Button.inline(
+            "{} {} {}".format(
+                Config.EMOJI_TO_DISPLAY_IN_HELP, x, Config.EMOJI_TO_DISPLAY_IN_HELP
+            ),
+            data="us_plugin_{}".format(x),
+        )
+        for x in helpable_plugins
+    ]
     if number_of_cols == 1:
         pairs = list(zip(modules[::number_of_cols]))
     elif number_of_cols == 2:
@@ -113,18 +155,30 @@ def paginate_help(page_number, loaded_plugins, prefix):
             zip(
                 modules[::number_of_cols],
                 modules[1::number_of_cols],
-                modules[2::number_of_cols],))
+                modules[2::number_of_cols],
+            )
+        )
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     elif len(modules) % number_of_cols == 2:
         pairs.append((modules[-2], modules[-1]))
-max_num_pages = ceil(len(pairs) / number_of_rows)
-    modulo_page = current_page_number % max_num_pages
+    max_num_pages = math.ceil(len(pairs) / number_of_rows)
+    modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
-            [
-            (custom.Button.inline("⏮️", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("❌", data="close"),
-             custom.Button.inline("⏭️", data="{}_next({})".format(prefix, modulo_page)))
+        pairs = pairs[
+            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "⌫", data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline("Close", data="close"),
+                custom.Button.inline(
+                    "⌦", data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
         ]
     return pairs
+
+
+
