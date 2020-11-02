24831369c076990f1fc690cd547e89e01657c069
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 07:20:18 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -94,9 +94,12 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
-    helpable_plugins = [p for p in loaded_plugins if not p.startswith("_")]
+    helpable_plugins = []
+    for p in loaded_plugins:
+        if not p.startswith("_"):
+            helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
-    modules = [
+     modules = [
         custom.Button.inline(
             "{} {} {}".format(
                 Config.EMOJI_TO_DISPLAY_IN_HELP, x, Config.EMOJI_TO_DISPLAY_IN_HELP
