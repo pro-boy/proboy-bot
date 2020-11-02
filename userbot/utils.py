1568b2917c7d1bab3b32f2a871645503663884cf
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 19:17:54 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -373,17 +373,6 @@ def humanbytes(size):
 
 
 
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    """ run command in terminal """
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(*args,
-                                                   stdout=asyncio.subprocess.PIPE,
-                                                   stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    return (stdout.decode('utf-8', 'replace').strip(),
-            stderr.decode('utf-8', 'replace').strip(),
-            process.returncode,
-            process.pid)
 
 
 async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Optional[str]:
