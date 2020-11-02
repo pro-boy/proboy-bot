b4873c786e0bfb9f5aea99dcd00572a5d91440f4
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-02 16:13:59 +0530

    Update glitch.py

--- userbot/plugins/glitch.py
+++ userbot/plugins/glitch.py
@@ -1,121 +1,110 @@
 """
 designed By @Krishna_Singhal in userge
 ported to telethon by @mrconfused and @sandy1709
+imported by @Nitesh_231 for USERBOT kek :) @heyworld roks🤘
 """
 
 import os
-
 from PIL import Image
-from telethon import functions, types
+from userbot.utils import take_screen_shot, runcmd
+from glitch_this import ImageGlitcher
+from userbot import bot, CMD_HELP, LOGS
+from userbot.events import register
 
-from .. import CMD_HELP, LOGS
-from ..utils import admin_cmd, edit_or_reply
 
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="(glitch|glitchs)(?: |$)(.*)"))
-async def glitch(cat):
-    cmd = cat.pattern_match.group(1)
-    catinput = cat.pattern_match.group(2)
-    reply = await cat.get_reply_message()
-    catid = cat.reply_to_msg_id
-    cat = await edit(cat, "```Glitching... 😁```")
+@register(pattern="^.(glitch|glitchs)(?: |$)(.*)", outgoing=True)
+async def glitch(event):
+    await event.edit("```Glitching Wait...```")
+    cmd = event.pattern_match.group(1)
+    input = event.pattern_match.group(2)
+    reply = await event.get_reply_message()
     if not (reply and (reply.media)):
-        await cat.edit("`Media not found...`")
+        await event.edit("`Media not found...`")
         return
     if not os.path.isdir("./temp/"):
         os.mkdir("./temp/")
-    catsticker = await reply.download_media(file="./temp/")
-    if not catsticker.endswith((".mp4", ".webp", ".tgs", ".png", ".jpg")):
-        os.remove(catsticker)
-        await cat.edit("`Media not found...`")
+    reply_to_id = event.reply_to_msg_id
+    remixsticker = await reply.download_media(file="./temp/")
+    if not remixsticker.endswith(('.mp4', '.webp', '.tgs', '.png', '.jpg')):
+        os.remove(remixsticker)
+        await event.edit("`Media not found...`")
         return
-    os.path.join("./temp/", "glitch.png")
-    if catinput:
-        if not catinput.isdigit():
-            await cat.edit("`You input is invalid, check help`")
+    file = os.path.join("./temp/", "glitch.png")
+    if input:
+        if not input.isdigit():
+            await event.edit("`You input is invalid, check help`")
             return
-        catinput = int(catinput)
-        if not 0 < catinput < 9:
-            await cat.edit("`Invalid Range...`")
+        input = int(input)
+        if not 0 < input < 9:
+            await event.edit("`Invalid Range...`")
             return
     else:
-        catinput = 2
-    if catsticker.endswith(".tgs"):
-        catfile = os.path.join("./temp/", "glitch.png")
-        catcmd = (
-            f"lottie_convert.py --frame 0 -if lottie -of png {catsticker} {catfile}"
-        )
-        stdout, stderr = (await runcmd(catcmd))[:2]
-        if not os.path.lexists(catfile):
-            await cat.edit("`catsticker not found...`")
+        input = 2
+    if remixsticker.endswith(".tgs"):
+        file = os.path.join("./temp/", "glitch.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {remixsticker} {file}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        if not os.path.lexists(file):
+            await event.edit("`remixsticker not found...`")
             LOGS.info(stdout + stderr)
-        glitch_file = catfile
-    elif catsticker.endswith(".webp"):
-        catfile = os.path.join("./temp/", "glitch.png")
-        os.rename(catsticker, catfile)
-        if not os.path.lexists(catfile):
-            await cat.edit("`catsticker not found... `")
+        glitch_file = file
+    elif remixsticker.endswith(".webp"):
+        file = os.path.join("./temp/", "glitch.png")
+        os.rename(remixsticker, file)
+        if not os.path.lexists(file):
+            await event.edit("`remixsticker not found... `")
             return
-        glitch_file = catfile
-    elif catsticker.endswith(".mp4"):
-        catfile = os.path.join("./temp/", "glitch.png")
-        await take_screen_shot(catsticker, 0, catfile)
-        if not os.path.lexists(catfile):
-            await cat.edit("```catsticker not found...```")
+        glitch_file = file
+    elif remixsticker.endswith(".mp4"):
+        file = os.path.join("./temp/", "glitch.png")
+        await take_screen_shot(remixsticker, 0, file)
+        if not os.path.lexists(file):
+            await event.edit("```remixsticker not found...```")
             return
-        glitch_file = catfile
+        glitch_file = file
     else:
-        glitch_file = catsticker
+        glitch_file = remixsticker
     glitcher = ImageGlitcher()
     img = Image.open(glitch_file)
     if cmd == "glitchs":
         glitched = "./temp/" + "glitched.webp"
-        glitch_img = glitcher.glitch_image(img, catinput, color_offset=True)
+        glitch_img = glitcher.glitch_image(img, input, color_offset=True)
         glitch_img.save(glitched)
-        await cat.client.send_file(cat.chat_id, glitched, reply_to=catid)
+        await bot.send_file(
+            event.chat_id,
+            glitched,
+            reply_to_message_id=reply_to_id)
         os.remove(glitched)
-        await cat.delete()
+        await event.delete()
     elif cmd == "glitch":
         Glitched = "./temp/" + "glitch.gif"
-        glitch_img = glitcher.glitch_image(img, catinput, color_offset=True, gif=True)
+        glitch_img = glitcher.glitch_image(
+            img, input, color_offset=True, gif=True)
         DURATION = 200
         LOOP = 0
         glitch_img[0].save(
             Glitched,
-            format="GIF",
+            format='GIF',
             append_images=glitch_img[1:],
             save_all=True,
             duration=DURATION,
-            loop=LOOP,
-        )
-        sandy = await cat.client.send_file(cat.chat_id, Glitched, reply_to=catid)
-        await cat.client(
-            functions.messages.SaveGifRequest(
-                id=types.InputDocument(
-                    id=sandy.media.document.id,
-                    access_hash=sandy.media.document.access_hash,
-                    file_reference=sandy.media.document.file_reference,
-                ),
-                unsave=True,
-            )
-        )
+            loop=LOOP)
+        await bot.send_file(
+            event.chat_id,
+            Glitched,
+            reply_to_message_id=reply_to_id)
         os.remove(Glitched)
-        await cat.delete()
-    for files in (catsticker, glitch_file):
+        await event.delete()
+    for files in (remixsticker, glitch_file):
         if files and os.path.exists(files):
             os.remove(files)
 
-
-CMD_HELP.update(
-    {
-        "glitch": "**Plugin : **`glitch`\
-    \n\n**Syntax : **`.glitch` reply to media file\
-    \n**Usage :** glitches the given mediafile (gif , stickers , image, videos) to a gif and glitch range is from 1 to 8.\
-    If nothing is mentioned then by default it is 2\
-    \n\n**Syntax : **`.glitchs` reply to media file\
-    \n**Usage :** glitches the given mediafile (gif , stickers , image, videos) to a sticker and glitch range is from 1 to 8.\
-    If nothing is mentioned then by default it is 2\
-    "
-    }
-)
+CMD_HELP.update({
+    "glitch":
+    ".glitch` reply to media file\
+\nUsage:glitches the given mediafile(gif , stickers , image, videos) to a gif and glitch range is from 1 to 8.\
+If nothing is mentioned then by default it is 2\
+\n\n.glitchs reply to media file\
+\nUsage:glitches the given mediafile(gif , stickers , image, videos) to a sticker and glitch range is from 1 to 8.\
+If nothing is mentioned then by default it is 2."
+})
