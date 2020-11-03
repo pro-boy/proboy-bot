a9ceb65dc0bdb25b1067659218d864af1806267f
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-03 16:49:48 +0530

    Update memify.py

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -1,54 +1,71 @@
-# For The-TG-Bot-3.0
-# By Priyam Kalra
-# Parts of the code below is taken from other sources, the links to the
-# sources is commented above the taken code
+import os
+import textwrap
 
 from PIL import Image, ImageFont, ImageDraw
-import textwrap
-import os
-from userbot.events import register
-from userbot import (
-    CMD_HELP,
-    LOGS,
-    TEMP_DOWNLOAD_DIRECTORY,
-    bot,
-    BOTLOG_CHATID)
+
+from userge import userge, Message, Config
+from userge.utils import progress, take_screen_shot, runcmd
 
 
-@register(outgoing=True, pattern=r"\.memify ?(.*)")
-async def handler(event):
-    if event.fwd_from:
+@borg.on(admin_cmd(pattern=r"mmf", about={
+    'header': "Memify aka Geyify (๑¯ω¯๑)",
+    'description': "Write text on any gif/sticker/image. "
+                   "Top and bottom text are separated by ; \n Naw gu Awey",
+    'usage': "{tr}mmf [text on top] ; [text on bottom] as a reply.",
+    'examples': "Gwad who needs examples for this"})
+async def memify(message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.err("LMAO no one's gonna help you, if u use .help now then u **Gey**")
+        await message.client.send_sticker(
+            sticker="CAADAQADhAAD3gkwRviGxMVn5813FgQ", chat_id=message.chat.id)
         return
-    if not event.reply_to_msg_id:
-        await event.edit("You might want to try `.help memify`")
+    if not (replied.photo or replied.sticker or replied.animation):
+        await message.err("Bruh, U Comedy me? Read help or gtfo (¬_¬)")
         return
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("```Reply to a image/sticker.```")
-        return
-    file = await bot.download_media(reply_message)
-    await event.edit("```Memifying this image! (」ﾟﾛﾟ)｣ ```")
-    text = str(event.pattern_match.group(1)).strip()
-    if len(text) < 1:
-        return await event.edit("You might want to try `.help memify`")
-    meme = await drawText(file, text)
-    await bot.send_file(event.chat_id, file=meme, force_document=False)
-    os.remove(meme)
+    if not os.path.isdir(Config.DOWN_PATH):
+        os.makedirs(Config.DOWN_PATH)
+    await message.edit("He he, let me use my skills")
+    dls = await message.client.download_media(
+        message=message.reply_to_message,
+        file_name=Config.DOWN_PATH,
+        progress=progress,
+        progress_args=(message, "Trying to Posses given content")
+    )
+    dls_loc = os.path.join(Config.DOWN_PATH, os.path.basename(dls))
+    if replied.sticker and replied.sticker.file_name.endswith(".tgs"):
+        await message.edit("OMG, an Animated sticker ⊙_⊙, lemme do my bleck megik...")
+        png_file = os.path.join(Config.DOWN_PATH, "meme.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {dls_loc} {png_file}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        os.remove(dls_loc)
+        if not os.path.lexists(png_file):
+            await message.err("This sticker is Gey, i won't memify it ≧ω≦")
+            raise Exception(stdout + stderr)
+        dls_loc = png_file
+    elif replied.animation:
+        await message.edit("Look it's GF. Oh, no it's just a Gif ")
+        jpg_file = os.path.join(Config.DOWN_PATH, "meme.jpg")
+        await take_screen_shot(dls_loc, 0, jpg_file)
+        os.remove(dls_loc)
+        if not os.path.lexists(jpg_file):
+            await message.err("This Gif is Gey (｡ì _ í｡), won't memify it.")
+            return
+        dls_loc = jpg_file
+    await message.edit("Decoration Time ≧∇≦, I'm an Artist")
+    webp_file = await draw_meme_text(dls_loc, message.input_str)
+    await message.client.send_sticker(chat_id=message.chat.id,
+                                      sticker=webp_file,
+                                      reply_to_message_id=replied.message_id)
+    await message.delete()
+    os.remove(webp_file)
 
-# Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
-# Maybe edited to suit the needs of this module
 
-
-async def drawText(image_path, text):
+async def draw_meme_text(image_path, text):
     img = Image.open(image_path)
     os.remove(image_path)
-    shadowcolor = "black"
     i_width, i_height = img.size
-    if os.name == "nt":
-        fnt = "arialbd.ttf"
-    else:
-        fnt = "userbot/helpers/styles/impact.ttf"
-    m_font = ImageFont.truetype(fnt,int((70 / 640) * i_width))
+    m_font = ImageFont.truetype("resources/MutantAcademyStyle.ttf", int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
     else:
@@ -59,62 +76,42 @@ async def drawText(image_path, text):
     if upper_text:
         for u_text in textwrap.wrap(upper_text, width=15):
             u_width, u_height = draw.textsize(u_text, font=m_font)
-            draw.text(xy=(((i_width - u_width) / 2) - 2, int((current_h / 640)
-                                                             * i_width)), text=u_text, font=m_font, fill=(0, 0, 0))
-            draw.text(xy=(((i_width - u_width) / 2) + 2, int((current_h / 640)
-                                                             * i_width)), text=u_text, font=m_font, fill=(0, 0, 0))
-            draw.text(xy=((i_width - u_width) / 2,
-                          int(((current_h / 640) * i_width)) - 2),
-                      text=u_text,
-                      font=m_font,
-                      fill=(0,
-                            0,
-                            0))
-            draw.text(xy=(((i_width - u_width) / 2),
-                          int(((current_h / 640) * i_width)) + 2),
-                      text=u_text,
-                      font=m_font,
-                      fill=(0,
-                            0,
-                            0))
 
-            draw.text(xy=((i_width - u_width) / 2, int((current_h / 640)
-                                                       * i_width)), text=u_text, font=m_font, fill=(255, 255, 255))
+            draw.text(xy=(((i_width - u_width) / 2) - 1, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=(((i_width - u_width) / 2) + 1, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=((i_width - u_width) / 2, int(((current_h / 640)*i_width)) - 1),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+            draw.text(xy=(((i_width - u_width) / 2), int(((current_h / 640)*i_width)) + 1),
+                      text=u_text, font=m_font, fill=(0, 0, 0))
+
+            draw.text(xy=((i_width - u_width) / 2, int((current_h / 640)*i_width)),
+                      text=u_text, font=m_font, fill=(255, 255, 255))
             current_h += u_height + pad
     if lower_text:
         for l_text in textwrap.wrap(lower_text, width=15):
             u_width, u_height = draw.textsize(l_text, font=m_font)
+
             draw.text(
-                xy=(((i_width - u_width) / 2) - 2, i_height -
-                    u_height - int((20 / 640) * i_width)),
+                xy=(((i_width - u_width) / 2) - 1, i_height - u_height - int((20 / 640)*i_width)),
                 text=l_text, font=m_font, fill=(0, 0, 0))
             draw.text(
-                xy=(((i_width - u_width) / 2) + 2, i_height -
-                    u_height - int((20 / 640) * i_width)),
+                xy=(((i_width - u_width) / 2) + 1, i_height - u_height - int((20 / 640)*i_width)),
                 text=l_text, font=m_font, fill=(0, 0, 0))
             draw.text(
-                xy=((i_width - u_width) / 2, (i_height -
-                                              u_height - int((20 / 640) * i_width)) - 2),
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640)*i_width)) - 1),
                 text=l_text, font=m_font, fill=(0, 0, 0))
             draw.text(
-                xy=((i_width - u_width) / 2, (i_height -
-                                              u_height - int((20 / 640) * i_width)) + 2),
+                xy=((i_width - u_width) / 2, (i_height - u_height - int((20 / 640)*i_width)) + 1),
                 text=l_text, font=m_font, fill=(0, 0, 0))
 
             draw.text(
-                xy=((i_width - u_width) / 2, i_height -
-                    u_height - int((20 / 640) * i_width)),
+                xy=((i_width - u_width) / 2, i_height - u_height - int((20 / 640)*i_width)),
                 text=l_text, font=m_font, fill=(255, 255, 255))
             current_h += u_height + pad
+
     image_name = "memify.webp"
-    webp_file = os.path.join(image_name)
+    webp_file = os.path.join(Config.DOWN_PATH, image_name)
     img.save(webp_file, "webp")
     return webp_file
-
-
-CMD_HELP.update({
-    "memify": "\
-```.memify <text_to_be_pasted_on_top> ; <text_to_be_pasted_on_bottom>```\
-\nUsage: Memifies the image/sticker/gif.\
-"
-})
