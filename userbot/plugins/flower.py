8f526f61aa1a9e2bbe631add917d7d8da8fb7bdb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-03 09:16:46 +0530

    Update and rename meme.py to flower.py

---
+++ userbot/plugins/flower.py
@@ -0,0 +1,30 @@
+
+@borg.on(admin_cmd(pattern=r"flower"))
+async def meme(event):
+    if event.fwd_from:
+        return   
+    flower ="✨ 🌹✨"
+    sleepValue = 4
+           
+    await event.edit(flower+"        ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+"       ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+"      ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+"     ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+"    ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+flower+"   ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+flower+flower+"   ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+flower+flower+flower+"  ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+flower+flower+flower+flower+" ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(flower+flower+flower+flower+flower+flower+flower+flower+flower+flower)
+    await asyncio.sleep(sleepValue)
+        
+    
