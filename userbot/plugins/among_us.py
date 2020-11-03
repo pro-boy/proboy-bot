61f0488442da8a35e23ec81a1067bb58a49a0cfd
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-03 12:03:02 +0530

    Update among_us.py

--- userbot/plugins/among_us.py
+++ userbot/plugins/among_us.py
@@ -1,144 +1,99 @@
-"""
-Created by @Jisan7509
-Credit @Infinity20998
-Userbot plugin fot CatUserbot
-"""
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+# created by @danish_00 .. sp thanks @one4udark
+
+from io import BytesIO
+from random import choice, randint
+from textwrap import wrap
+
+from PIL import Image, ImageDraw, ImageFont
+from requests import get
+
+from userbot import CMD_HELP
+from userbot.events import register
 
 
-import asyncio
+@register(outgoing=True, pattern="^.saylie (.*)")
+async def f_load(message):
+    clrs = {
+        "red": 1,
+        "lime": 2,
+        "green": 3,
+        "blue": 4,
+        "cyan": 5,
+        "brown": 6,
+        "purple": 7,
+        "pink": 8,
+        "orange": 9,
+        "yellow": 10,
+        "white": 11,
+        "black": 12,
+    }
+    clr = randint(1, 12)
+    text = message.pattern_match.group(1)
+    reply = await message.get_reply_message()
+    if text in clrs:
+        clr = clrs[text]
+        text = None
+    if not text:
+        if not reply:
+            await bruh(message, message.sender)
+            return
+        if not reply.text:
+            await bruh(message, reply.sender)
+            return
+        text = reply.pattern_match.group(1)
 
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot import ALIVE_NAME, CMD_HELP
+    if text.split(" ")[0] in clrs:
+        clr = clrs[text.split(" ")[0]]
+        text = " ".join(text.split(" ")[1:])
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
-
-
-@bot.on(admin_cmd(pattern="imp(|n) (.*)", outgoing=True))
-async def _(event):
-    hmm = bot.uid
-    USERNAME = f"tg://user?id={hmm}"
-    name = event.pattern_match.group(2)
-    cmd = event.pattern_match.group(1).lower()
-    text1 = await event.reply("Uhmm... Something is wrong here!!")
-    await asyncio.sleep(2)
-    await text1.delete()
-    stcr1 = await event.client.send_file(
-        event.chat_id, "CAADAQADRwADnjOcH98isYD5RJTwAg"
-    )
-    text2 = await event.reply(
-        f"**[{DEFAULTUSER}]({USERNAME}) :** I have to call discussion"
-    )
-    await asyncio.sleep(3)
-    await stcr1.delete()
-    await text2.delete()
-    stcr2 = await event.client.send_file(
-        event.chat_id, "CAADAQADRgADnjOcH9odHIXtfgmvAg"
-    )
-    text3 = await event.reply(
-        f"**[{DEFAULTUSER}]({USERNAME}) :** We have to eject the imposter or will lose "
-    )
-    await asyncio.sleep(3)
-    await stcr2.delete()
-    await text3.delete()
-    stcr3 = await event.client.send_file(
-        event.chat_id, "CAADAQADOwADnjOcH77v3Ap51R7gAg"
-    )
-    text4 = await event.reply(f"**Others :** Where??? ")
-    await asyncio.sleep(2)
-    await text4.edit(f"**Others :** Who?? ")
-    await asyncio.sleep(2)
-    await text4.edit(
-        f"**[{DEFAULTUSER}]({USERNAME}) :** Its {name} , I saw {name}  using vent,"
-    )
-    await asyncio.sleep(3)
-    await text4.edit(f"**Others :**Okay.. Vote {name} ")
-    await asyncio.sleep(2)
-    await stcr3.delete()
-    await text4.delete()
-    stcr4 = await event.client.send_file(
-        event.chat_id, "CAADAQADLwADnjOcH-wxu-ehy6NRAg"
-    )
-    catevent = await event.reply(f"{name} is ejected.......")
-    await asyncio.sleep(2)
-    await catevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
-    await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
-    await asyncio.sleep(0.2)
-    await stcr4.delete()
-    if cmd == "":
-        await catevent.edit(
-            f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
+    if text == "colors":
+        await message.edit(
+            "Cores disponíveis:\n"
+            + ("\n".join([f"• `{i}`" for i in list(clrs.keys())]))
         )
-        await asyncio.sleep(4)
-        await catevent.delete()
-        await event.client.send_file(event.chat_id, "CAADAQADLQADnjOcH39IqwyR6Q_0Ag")
-    elif cmd == "n":
-        await catevent.edit(
-            f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was not an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         1 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
-        )
-        await asyncio.sleep(4)
-        await catevent.delete()
-        await event.client.send_file(event.chat_id, "CAADAQADQAADnjOcH-WOkB8DEctJAg")
+        return
+
+    url = "https://raw.githubusercontent.com/KeyZenD/AmongUs/master/"
+    font = ImageFont.truetype(BytesIO(get(url + "bold.ttf").content), 60)
+    imposter = Image.open(BytesIO(get(f"{url}{clr}.png").content))
+    text_ = "\n".join(["\n".join(wrap(part, 30)) for part in text.split("\n")])
+    w, h = ImageDraw.Draw(Image.new("RGB", (1, 1))).multiline_textsize(
+        text_, font, stroke_width=2
+    )
+    text = Image.new("RGBA", (w + 30, h + 30))
+    ImageDraw.Draw(text).multiline_text(
+        (15, 15), text_, "#FFF", font, stroke_width=2, stroke_fill="#000"
+    )
+    w = imposter.width + text.width + 10
+    h = max(imposter.height, text.height)
+    image = Image.new("RGBA", (w, h))
+    image.paste(imposter, (0, h - imposter.height), imposter)
+    image.paste(text, (w - text.width, 0), text)
+    image.thumbnail((512, 512))
+    output = BytesIO()
+    output.name = "imposter.webp"
+    image.save(output)
+    output.seek(0)
+    await message.delete()
+    await message.client.send_file(message.to_id, output, reply_to=reply)
 
 
-@bot.on(admin_cmd(pattern="timp(|n) (.*)", outgoing=True))
-async def _(event):
-    name = event.pattern_match.group(2)
-    cmd = event.pattern_match.group(1).lower()
-    catevent = await event.reply(f"{name} is ejected.......")
-    await asyncio.sleep(2)
-    await catevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
-    await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
-    await asyncio.sleep(0.2)
-    if cmd == "":
-        await catevent.edit(
-            f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ {name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
-        )
-    elif cmd == "n":
-        await catevent.edit(
-            f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ {name} was not an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         1 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
-        )
+async def bruh(message, user):
+    fn = user.first_name
+    ln = user.last_name
+    name = fn + (" " + ln if ln else "")
+    name = "***" + name
+    await message.edit(name + choice([" ", "Not"]) + "he was not an Imposter! ***")
 
 
 CMD_HELP.update(
     {
-        "imposter": "**Plugin :** `imposter__`\
-\n\n**Syntax : **`.imp` / `.impn` <text>\
-\n**Usage : ** Find imposter with stickers.\
-\n\n**Syntax : **`.timp` / `.timpn` <text>\
-\n**Usage : ** Find imposter only text."
+        "amongus": ".saylie\
+    \n Among us."
     }
+
 )
