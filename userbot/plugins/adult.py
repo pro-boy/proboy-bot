633223c6145d86bca97e0c59643c91b9992c3251
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-04 07:57:11 +0530

    Update adult.py

--- userbot/plugins/adult.py
+++ userbot/plugins/adult.py
@@ -1,5 +1,3 @@
-#port to userbot by @MoveAngel
-
 import datetime
 
 from telethon import events
@@ -14,26 +12,57 @@ from userbot.utils import admin_cmd
 
 #@register(outgoing=True, pattern="^.q(?: |$)(.*)")
 
-#@borg.on(admin_cmd(pattern=r"pbot(?: |$)(.*)"))
-@borg.on(admin_cmd(pattern="pbot ?(.*)"))
+@borg.on(admin_cmd(pattern=r"pbot(?: |$)(.*)"))
+
 async def _(event):
+
     if event.fwd_from:
+
         return 
+
     if not event.reply_to_msg_id:
-       await event.edit("Sir reply to sticker message")
+
+       await event.edit("```Reply to any user message.```")
+
        return
+
     reply_message = await event.get_reply_message() 
-    
+
+    if not reply_message.text:
+
+       await event.edit("```Reply to text message```")
+
+       return
+
     chat = "@Epornerbot"
-    await event.edit("Making a image")
-    async with event.client.conversation(chat) as conv:
+
+    sender = reply_message.sender
+
+    if reply_message.sender.bot:
+
+       await event.edit("```Reply to actual users message.```")
+
+       return
+
+    await event.edit("```Making a Quote```")
+
+    async with bot.conversation(chat) as conv:
+
           try:     
+
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=105460780))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
+
+              await bot.forward_messages(chat, reply_message)
+              response = await conv.get_response()
+              respond = await conv.get_response()
+
+            
           except YouBlockedUserError: 
-              await event.reply("Unblock me (@stickers_to_image_bot) to work")
+
+              await event.reply("```Please unblock @QuotLyBot and try again```")
+
               return
+
           if response.text.startswith("I understand only stickers"):
               await event.edit("Sorry i cant't convert it check wheter is non animated sticker or not")
           else:
