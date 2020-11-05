2e22b8534b033158d26735a71745af41e813c61a
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-05 20:38:06 +0530

    Update reverse.py

--- userbot/plugins/reverse.py
+++ userbot/plugins/reverse.py
@@ -22,7 +22,7 @@ useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) 
 opener.addheaders = [('User-agent', useragent)]
 
 
-@register(outgoing=True, pattern=r"^.imgsearch(?: |$)(\d*)")
+@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
 @errors_handler
 async def okgoogle(img):
     """ For .reverse command, Google search images and stickers. """
