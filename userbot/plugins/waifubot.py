2ed89e8026dffb7d5b69452f4d0106fc7799831c
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-06 06:02:12 +0530

    Update waifubot.py

--- userbot/plugins/waifubot.py
+++ userbot/plugins/waifubot.py
@@ -26,7 +26,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    BASE_URL = "https://myanimelist.net/"
+    BASE_URL = "https://images.search.yahoo.com/"
     OUTPUT_STR = "Reply to an image to do Google Reverse Search"
     if event.reply_to_msg_id:
         
