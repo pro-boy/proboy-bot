cf90c53a8b5c2c58aed9fa63a5b5fb501b8feefb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-06 21:52:42 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -72,6 +72,7 @@ if ENV:
         #heroku 
         HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
         HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+       PRIVATE_CHANNEL_BOT_API_ID = os.environ.get("PRIVATE_CHANNEL_BOT_API_ID", None)
         # send .get_id in any channel to forward all your NEW PMs to this group
         PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
         if PRIVATE_GROUP_BOT_API_ID:
