419b8a5b2a2dfc924738a93cc053d07d1d46ce92
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-08 00:39:28 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,16 +1,49 @@
-import asyncio	
-from telethon import events	
-from telethon.tl.types import ChannelParticipantsAdmins	
-from platform import uname	
-from userbot import ALIVE_NAME	
-from userbot.utils import admin_cmd	
+import platform
+import shutil
+import sys
+import time
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from datetime import datetime
+from os import remove
+from platform import python_version, uname
+from shutil import which
+
+import psutil
+from telethon import __version__, version
+
+from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP, USERBOT_VERSION, StartTime, bot
+from userbot.events import register
+	
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"	
+modules = CMD_HELP
 
-@command(outgoing=True, pattern="^.alive$")	
+@command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):	
-    """ For .alive command, check if the bot is running.  """	
-    await alive.edit("`░█─░█ █▀▀ █── █── █▀▀█ \n░█▀▀█ █▀▀ █── █── █──█ \n░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ \n\nYes Sir ! I'm Alive\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"	
-                    f"`My Master`: {DEFAULTUSER}\n"	
-                     "`My Owner`: @danish_00\n\n"	
-                     "Join [Channel](https://t.me/joinchat/AAAAAFDU0O-jAUqEaWm-bg) For Latest Updates")	
+uptime = await get_readable_time((time.time() - StartTime))
+    output = (
+        "`Bot is running smoothly 😎🏓...`\n"
+        "`▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
+        f"»✳️>⚙️ `Telethon     : v{version.__version__} `\n"
+        f"»✳️>🐍 `Python       : v{python_version()} `\n"
+        f"»✳️>👤 `User`        : {DEFAULTUSER}\n"
+        "`▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱`\n"
+        f"»✳️>💻 `Running on   :`[Hidden Repo](t.me/danish_00)\n"
+        f"»✳️>🗃 `New Plugins  : {len(modules)} `\n"
+        f"»✳️>🤖 `DanishBot    : v{USERBOT_VERSION} `\n"
+        f"»✳️>⏱️ `Bot Uptime   : {uptime} `\n"
+        "`▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`"
+    )
+       if ALIVE_LOGO:
+          try:
+            logo = ALIVE_LOGO
+            await bot.send_file(alive.chat_id, logo, caption=output)
+            await alive.delete()
+        except BaseException:
+            await alive.edit(
+                output + "\n\n *`The provided logo is invalid."
+                "\nMake sure the link is directed to the logo picture`"
+            )
+    else:
+        await alive.edit(output)
