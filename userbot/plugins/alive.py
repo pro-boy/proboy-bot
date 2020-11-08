490e8b6d4084fb5e3458808a04389bdef835632c
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-08 09:58:55 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,1 +1,12 @@
+from userbot import CMD_HELP
 
+CMD_HELP.update(
+    {
+        "alive": ".alive\
+    \nUsage: Type .alive to see if your bot works or not.\
+    \n\n.aliveu <text>\
+    \nUsage: Change 'alive' user to the text you want.\
+    \n\n.resetalive\
+    \nUsage: Reset user to default."
+    }
+)
