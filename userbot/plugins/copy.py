de1d1ae6e67ccd6fea1d07ab43c8329e1a1bcf2c
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-09 14:48:59 +0530

    Update and rename whatscrap.py to copy.py

--- userbot/plugins/copy.py
+++ userbot/plugins/copy.py
@@ -11,8 +11,8 @@ async def _(event):
         previous_message = await event.get_reply_message()
         the_real_message = previous_message.text
         reply_to_id = event.reply_to_msg_id
-        the_real_message = the_real_message.replace("*", "**")
-        the_real_message = the_real_message.replace("_", "__")
+        the_real_message = the_real_message.replace("*", "*")
+        the_real_message = the_real_message.replace("_", "_")
         await event.edit(the_real_message)
     else:
         await event.edit("Reply to a message with `.copy` to copy it")
