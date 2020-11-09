40ad9c38f412ed3632c3b38f6110e1651f7a59dc
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-09 15:37:49 +0530

    Add files via upload

---
+++ userbot/plugins/trojan.py
@@ -0,0 +1,323 @@
+
+
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, CMD_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.trojan(?: |$)(.*)")
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	await typew.edit("`Trojan is thrown ...`")
+	sleep(4)
+	await typew.edit("0%")
+	number = 1
+	await typew.edit(str(number) + "%   ?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -----------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   -------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   --------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ---------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------------")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------------?")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(str(number) + "%   ----------------?")
+	sleep(1)
+	await typew.edit("Trojan throwing successful.")
+	#
+
+CMD_HELP.update({
+    "trojan":
+    ".trojan\
+    ."
+})
