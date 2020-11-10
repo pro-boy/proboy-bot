446ebe0cd00398b15b14ac72bcda6d6f0e4383c4
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-10 17:57:53 +0530

    Update filters.py

--- userbot/plugins/filters.py
+++ userbot/plugins/filters.py
@@ -1,145 +1,135 @@
-""" Userbot module for filter commands """
+# ported from paperplaneExtended by avinashreddy3108 for media support
+import re
 
-from asyncio import sleep
-from re import fullmatch, IGNORECASE, escape
-from userbot import CMD_HELP
-from userbot.events import register
+from telethon import events
+
+from .. import BOTLOG_CHATID, CMD_HELP, bot
+from ..utils import admin_cmd
+from .sql_helper.filter_sql import (
+    add_filter,
+    get_filters,
+    remove_all_filters,
+    remove_filter,
+)
 
 
-@register(incoming=True, disable_edited=False)
+@borg.on(events.NewMessage(incoming=True))
 async def filter_incoming_handler(handler):
-    """ Checks if the incoming message contains handler of a filter """
     try:
         if not (await handler.get_sender()).bot:
-            try:
-                from userbot.plugins.sql_helper.filter_sql import get_filters
-            except AttributeError:
-                await handler.edit("`Running on Non-SQL mode!`")
-                return
             name = handler.raw_text
             filters = get_filters(handler.chat_id)
             if not filters:
                 return
             for trigger in filters:
-                pro = fullmatch(trigger.keyword, name, flags=IGNORECASE)
-                if pro and trigger.f_mesg_id:
-                    msg_o = await handler.client.get_messages(
-                        entity=PRIVATE_GROUP_BOT_API_ID, ids=int(trigger.f_mesg_id))
-                    await handler.reply(msg_o.message, file=msg_o.media)
-                elif pro and trigger.reply:
-                    await handler.reply(trigger.reply)
+                pattern = r"( |^|[^\w])" + re.escape(trigger.keyword) + r"( |$|[^\w])"
+                if re.search(pattern, name, flags=re.IGNORECASE):
+                    if trigger.f_mesg_id:
+                        msg_o = await handler.client.get_messages(
+                            entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id)
+                        )
+                        await handler.reply(msg_o.message, file=msg_o.media)
+                    elif trigger.reply:
+                        await handler.reply(trigger.reply)
     except AttributeError:
         pass
 
 
-@register(outgoing=True, pattern=r"^.filter ((@)?\w*)")
+@borg.on(admin_cmd(pattern="filter (.*)"))
 async def add_new_filter(new_handler):
-    """ For .filter command, allows adding new filters in a chat """
-    try:
-        from userbot.plugins.sql_helper.filter_sql import add_filter
-    except AttributeError:
-        await new_handler.edit("`Running on Non-SQL mode!`")
-        return
     keyword = new_handler.pattern_match.group(1)
     string = new_handler.text.partition(keyword)[2]
     msg = await new_handler.get_reply_message()
     msg_id = None
     if msg and msg.media and not string:
-        if PRIVATE_GROUP_BOT_API_ID:
+        if BOTLOG_CHATID:
             await new_handler.client.send_message(
-                PRIVATE_GROUP_BOT_API_ID, f"#FILTER\nCHAT ID: {new_handler.chat_id}\nTRIGGER: {keyword}"
-                "\n\nThe following message is saved as the filter's reply data for the chat, please do NOT delete it !!"
+                BOTLOG_CHATID,
+                f"#FILTER\
+            \nCHAT ID: {new_handler.chat_id}\
+            \nTRIGGER: {keyword}\
+            \n\nThe following message is saved as the filter's reply data for the chat, please do NOT delete it !!",
             )
             msg_o = await new_handler.client.forward_messages(
-                entity=PRIVATE_GROUP_BOT_API_ID,
+                entity=BOTLOG_CHATID,
                 messages=msg,
                 from_peer=new_handler.chat_id,
-                silent=True)
+                silent=True,
+            )
             msg_id = msg_o.id
         else:
-            return await new_handler.edit(
-                "`Saving media as reply to the filter requires the PRIVATE_GROUP_BOT_API_ID to be set.`"
+            await 
+                new_handler.edit(
+                "`Saving media as reply to the filter requires the BOTLOG_CHATID to be set.`",
             )
+            return
     elif new_handler.reply_to_msg_id and not string:
         rep_msg = await new_handler.get_reply_message()
         string = rep_msg.text
     success = "`Filter` **{}** `{} successfully`"
     if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
-        await new_handler.edit(success.format(keyword, 'added'))
-    else:
-        await new_handler.edit(success.format(keyword, 'updated'))
+        return await new_handler.edit(success.format(keyword, "added"))
+    remove_filter(str(new_handler.chat_id), keyword)
+    if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
+        return await new_handler.edit(success.format(keyword, "Updated"))
+    await new_handler.edit(f"Error while setting filter for {keyword}")
 
 
-@register(outgoing=True, pattern=r"^.stop ((@)?\w*)")
-async def remove_a_filter(r_handler):
-    """ For .stop command, allows you to remove a filter from a chat. """
-    try:
-        from userbot.plugins.sql_helper.filter_sql import remove_filter
-    except AttributeError:
-        return await r_handler.edit("`Running on Non-SQL mode!`")
-    filt = r_handler.pattern_match.group(1)
-    if not remove_filter(r_handler.chat_id, filt):
-        await r_handler.edit("`Filter` **{}** `doesn't exist.`".format(filt))
-    else:
-        await r_handler.edit(
-            "`Filter` **{}** `was deleted successfully`".format(filt))
-
-
-@register(outgoing=True, pattern="^.rmbotfilters (.*)")
-async def kick_marie_filter(event):
-    """ For .rmfilters command, allows you to kick all \
-        Marie(or her clones) filters from a chat. """
-    cmd = event.text[0]
-    bot_type = event.pattern_match.group(1).lower()
-    if bot_type not in ["marie", "rose"]:
-        return await event.edit("`That bot is not yet supported!`")
-    await event.edit("```Will be kicking away all Filters!```")
-    await sleep(3)
-    resp = await event.get_reply_message()
-    filters = resp.text.split("-")[1:]
-    for i in filters:
-        if bot_type.lower() == "marie":
-            await event.reply("/stop %s" % (i.strip()))
-        if bot_type.lower() == "rose":
-            i = i.replace('`', '')
-            await event.reply("/stop %s" % (i.strip()))
-        await sleep(0.3)
-    await event.respond(
-        "```Successfully purged bots filters yaay!```\n Gimme cookies!")
-    if BOTLOG:
-        await event.client.send_message(
-            PRIVATE_GROUP_BOT_API_ID, "I cleaned all filters at " + str(event.chat_id))
-
-
-@register(outgoing=True, pattern="^.filters$")
-async def filters_active(event):
-    """ For .filters command, lists all of the active filters in a chat. """
-    try:
-        from userbot.plugins.sql_helper.filter_sql import get_filters
-    except AttributeError:
-        return await event.edit("`Running on Non-SQL mode!`")
-    transact = "`There are no filters in this chat.`"
+@borg.on(admin_cmd(pattern="filters$"))
+async def on_snip_list(event):
+    OUT_STR = "There are no filters in this chat."
     filters = get_filters(event.chat_id)
     for filt in filters:
-        if transact == "`There are no filters in this chat.`":
-            transact = "Active filters in this chat:\n"
-            transact += "`{}`\n".format(filt.keyword)
-        else:
-            transact += "`{}`\n".format(filt.keyword)
+        if OUT_STR == "There are no filters in this chat.":
+            OUT_STR = "Active filters in this chat:\n"
+        OUT_STR += "👉 `{}`\n".format(filt.keyword)
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "filters.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="Available Filters in the Current Chat",
+                reply_to=event,
+            )
+            await event.delete()
+    else:
+        await event.edit(OUT_STR)
 
-    await event.edit(transact)
+
+@borg.on(admin_cmd(pattern="stop (.*)"))
+async def remove_a_filter(r_handler):
+    filt = r_handler.pattern_match.group(1)
+    if not remove_filter(r_handler.chat_id, filt):
+        await r_handler.edit("Filter` {} `doesn't exist.".format(filt))
+    else:
+        await r_handler.edit("Filter `{} `was deleted successfully".format(filt))
 
 
-CMD_HELP.update({
-    "filter":
-    ">`.filters`"
-    "\nUsage: Lists all active userbot filters in a chat."
-    "\n\n>`.filter <keyword> <reply text>` or reply to a message with >`.filter <keyword>`"
-    "\nUsage: Saves the replied message as a reply to the 'keyword'."
-    "\nThe bot will reply to the message whenever 'keyword' is mentioned."
-    "\nWorks with everything from files to stickers."
-    "\n\n>`.stop <filter>`"
-    "\nUsage: Stops the specified filter."
-    "\n\n>`.rmbotfilters <marie/rose>`"
-    "\nUsage: Removes all filters of admin bots (Currently supported: Marie, Rose and their clones.) in the chat."
-})
+@borg.on(admin_cmd(pattern="rmfilters$"))
+async def on_all_snip_delete(event):
+    filters = get_filters(event.chat_id)
+    if filters:
+        remove_all_filters(event.chat_id)
+        await event.edit( f"filters in current chat deleted successfully")
+    else:
+        await event.edit( f"There are no filters in this group")
+
+
+CMD_HELP.update(
+    {
+        "filters": "**Plugin :**`filters`\
+    \n\n**Synatx :** `.filters`\
+    \n**Usage: **Lists all active (of your userbot) filters in a chat.\
+    \n\n*Synatx :** `.filter`  reply to a message with .filter <keyword>\
+    \n**Usage: **Saves the replied message as a reply to the 'keyword'.\
+    \nThe bot will reply to the message whenever 'keyword' is mentioned.\
+    \nWorks with everything from files to stickers.\
+    \n\n*Synatx :** `.stop <keyword>`\
+    \n**Usage: **Stops the specified keyword.\
+    \n\n*Synatx :** `.rmfilters` \
+    \n**Usage: **Removes all filters of your userbot in the chat."
+    }
+)
