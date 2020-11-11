540c081e54f7788ab0fc9b491b547ae7ff267d6b
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-11 15:40:12 +0530

    Update mmf2.py

--- userbot/plugins/mmf2.py
+++ userbot/plugins/mmf2.py
@@ -1,6 +1,5 @@
 import os
 import asyncio
-from .. import tempmemes
 from userbot import LOGS , CMD_HELP
 from userbot.helpers import take_screen_shot ,runcmd, convert_toimage
 from userbot.utils import admin_cmd
