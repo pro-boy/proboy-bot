f475b4a9668a4e5fadf97c6246aaadf6b1fa030a
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-12 21:28:49 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -684,7 +684,7 @@ async def date_func(dat):
 
 
 
-@borg.on(admin_cmd(pattern=r".isong (.*)"))
+@borg.on(admin_cmd(pattern=r"isong (.*)"))
 async def download_video(v_url):  
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
     if not sender.id == me.id:
