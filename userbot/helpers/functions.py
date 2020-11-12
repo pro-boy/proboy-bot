19683fc99724f8bf9384251b3f61bdf8726dff33
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-13 00:10:57 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -266,15 +266,15 @@ async def moditweet(text):
  async def miatweet(text):
         r = requests.get(
             f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=miakhalifa").json()
-        sandy = r.get("message")
-        caturl = url(sandy)
-        if not caturl:
+        wew = r.get("message")
+        hburl = url(wew)
+        if not hburl:
             return  "check syntax once more"
         with open("temp.png", "wb") as f:
-            f.write(requests.get(sandy).content)
+            f.write(requests.get(wew).content)
         img = Image.open("temp.png").convert("RGB")
         img.save("temp.webp", "webp")    
-        return "temp.webp"     
+        return "temp.webp"   
    
 async def tweets(text1,text2):
         r = requests.get(
