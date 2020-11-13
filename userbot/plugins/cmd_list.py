fda59edc8dbf28b043d1a9d16644ec33566433b8
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-13 12:27:27 +0530

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -17,4 +17,17 @@ async def install(event):
     _o = o.split("\n")
     o = "\n".join(_o)
     OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly. Visit__ @danish_00 __for assistance.__"
+   if len(OUTPUT) > 4095:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "cmd_list.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id,
+            )
+            await event.delete()
     await event.edit(OUTPUT)
+ await event.edit(OUTPUT)
