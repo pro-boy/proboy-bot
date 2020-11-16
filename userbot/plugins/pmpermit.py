c6e766ba32f116191ee2ba34d0b093903846a99c
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-16 15:27:49 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -4,8 +4,9 @@ import io
 from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
-from ..utils import admin_cmd
-from . import ALIVE_NAME, CMD_HELP, PM_START, PMMENU, check
+from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.plugins._init_ import PM_START, PMMENU, check
 from userbot.plugins.sql_helper import pmpermit_sql as pmpermit_sql
 
 PM_WARNS = {}
