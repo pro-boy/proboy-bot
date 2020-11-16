9f9619c1f0d522bbcc7fd7c361e13ed0eb928fbe
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-16 14:29:02 +0530

    😑

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -4,7 +4,7 @@ import io
 from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
-from Userbot.utils import admin_cmd
+from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME, CMD_HELP, PM_START, PMMENU, check
 from userbot.plugins.sql_helper import pmpermit_sql as pmpermit_sql
 
