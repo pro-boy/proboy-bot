a14f7ac40beb2b965be2d7532c08d4f38fca9aa4
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-16 15:21:43 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -10,7 +10,7 @@ from telethon import events, functions
 
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 
-from userbot import ALIVE_NAME, PM_START
+from . import ALIVE_NAME, PM_START
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Master"
 PREV_REPLY_MESSAGE = {}
