c70dc326f313e1a4d055d4a2b71ae2bb9429d103
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-16 15:28:43 +0530

    Update log_chats.py

--- userbot/plugins/log_chats.py
+++ userbot/plugins/log_chats.py
@@ -7,7 +7,7 @@ import userbot.plugins.sql_helper.no_log_pms_sql as no_log_pms_sql
 
 from userbot.utils import admin_cmd
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, LOGS
-from userbot.plugins import  mentionuser
+from userbot import  mentionuser
 RECENT_USER = None
 NEWPM = None
 COUNT = 0
