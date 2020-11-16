951e07cf7749af0dc2c98583ec8211d4080af93d
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-16 15:04:17 +0530

    Update _init_.py

--- userbot/plugins/_init_.py
+++ userbot/plugins/_init_.py
@@ -7,7 +7,7 @@ import heroku3
 import requests
 import spamwatch as spam_watch
 from validators.url import url
-
+from userbot import bot
 from userbot.helpers import *
 from userbot.uniborgConfig import Config
 
