7a5c7f1b2f526eadb93e862677c7ca263b6e0931
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-18 00:45:43 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -6,8 +6,8 @@ from os import environ, execle, path, remove
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from ..utils import admin_cmd
-from . import CMD_HELP, runcmd
+from userbot.utils import admin_cmd, runcmd
+from userbot import CMD_HELP 
 
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 HEROKU_API_KEY = Var.HEROKU_API_KEY
