2243a859ed785dc16e5e20157d17ba4266ebb58a
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-17 18:38:55 +0530

    Update whisperbot.py

--- userbot/plugins/whisperbot.py
+++ userbot/plugins/whisperbot.py
@@ -1,9 +1,41 @@
+from telegraph import Telegraph
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP
+
+from userbot import CMD_HELP, var
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="whisper ?(.*)"))
+telegraph = Telegraph()
+mee = telegraph.create_account(short_name="yohohehe")
+
+
+@borg.on(admin_cmd(pattern="instadl ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("**Reply to a instagram url.**")
+        return
+    reply_message = await event.get_reply_message()
+    chat = "@instadownloadingbot"
+    reply_message.sender
+    await event.edit("**Downloading the post...**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1310260390)
+            )
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.edit("```Please unblock me (@instadownloadingbot) u Nigga```")
+            return
+        await event.delete()
+        await event.client.send_message(
+            event.chat_id, response.message, reply_to=reply_message
+        )
+
+@borg.on(admin_cmd(pattern="wspr ?(.*)"))
 async def wspr(event):
     if event.fwd_from:
         return
@@ -90,8 +122,12 @@ async def _(event):
             "Downloaded sar 😎😎"
         )
 
-
 CMD_HELP.update(
     {
-        "botfns": "\n\n.whisper <message> <target username/id>\nUse - Send a whisper message to that person.\
-        
+        "bots": ".purl <reply to file>\nUse - Get a direct download link of that file/doc/pic/vid\
+        \n\n.instadl <reply to instagram url>\\nUse - Download that instagram post.\
+        \n\n.wspr <message> <target username/id>\nUse - Send a whisper message to that person.\
+        \n\n.checkspam\nUse - Check if you are limited.\
+        \n\n.gitdl <reply to github link>\nUse - Download the main branch of that git repo."
+    }
+)
