72d12b3171ea07a6ae4d7867b13b4a89ebe98c32
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-18 00:30:46 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -141,7 +141,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
         pairs = list(zip(modules[::number_of_cols]))
     elif number_of_cols == 2:
         pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    elif number_of_cols == 3:
+    else 
         pairs = list(
             zip(
                 modules[::number_of_cols],
@@ -149,21 +149,11 @@ def paginate_help(page_number, loaded_plugins, prefix):
                 modules[2::number_of_cols]
             )
         )
-     else  
-            pairs = list(
-            zip(
-                modules[::number_of_cols],
-                modules[1::number_of_cols],
-                modules[2::number_of_cols],
-                modules[3:: number_of_cols],
-            )
-        )
+     
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     elif len(modules) % number_of_cols == 2:
         pairs.append((modules[-2], modules[-1]))
-    elif len(modules) % number_of_cols == 3:
-        pairs.append((modules[-3], modules[-2], modules[-1]))
     max_num_pages = math.ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
