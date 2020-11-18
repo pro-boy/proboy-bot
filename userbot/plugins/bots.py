f62d54af51a2ffd0106a2220c380c7da2518b204
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-18 18:29:38 +0530

    Update bots.py

--- userbot/plugins/bots.py
+++ userbot/plugins/bots.py
@@ -1,13 +1,53 @@
+
+
+import datetime
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telegraph import Telegraph
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-
 from userbot import CMD_HELP
 from userbot.utils import admin_cmd
 
 telegraph = Telegraph()
 mee = telegraph.create_account(short_name="yohohehe")
 
+
+
+@borg.on(admin_cmd(pattern="recognize ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("Reply to any user's media message.")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("reply to media file")
+       return
+    chat = "@Rekognition_Bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("Reply to actual users message.")
+       return
+    cat = await event.edit("recognizeing this media")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.edit("unblock @Rekognition_Bot and try again")
+              await cat.delete()
+              return
+          if response.text.startswith("See next message."):
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              response = await response
+              cat = response.message.message
+              await event.edit(cat)
+      
+          else:
+              await event.edit("sorry, I couldnt find it")
+              
 
 @borg.on(admin_cmd(pattern="instadl ?(.*)"))
 async def _(event):
@@ -99,6 +139,7 @@ CMD_HELP.update(
     {
         "bots": ".purl <reply to file>\nUse - Get a direct download link of that file/doc/pic/vid\
         \n\n.instadl <reply to instagram url>\\nUse - Download that instagram post.\
+        \n\n.recognize <reply to any media>\\nUse - send details about it.\
         \n\n.wspr <message> <target username/id>\nUse - Send a whisper message to that person.\
         \n\n.limits\nUse - Check if you are limited by telegram.\
         \n\n.gitdl <reply to github link>\nUse - Download the main branch of that git repo."
