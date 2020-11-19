ea06d1121313c8b50f436b842fbafe204001e4ea
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-19 19:20:56 +0530

    Create troll.py

---
+++ userbot/plugins/troll.py
@@ -0,0 +1,144 @@
+"""
+Created by @Jisan7509
+plugin for Cat_Userbot
+☝☝☝
+You remove this, you gay.
+"""
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import CMD_HELP, bot
+
+from userbot.utils import admin_cmd
+
+
+@bot.on(admin_cmd(pattern="fox ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    jisan = event.pattern_match.group(1)
+    sf = f"sf"
+    cat = await event.edit("```Fox is on your way...```")
+    async with event.client.conversation("@themememakerbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=740813545)
+            )
+            await conv.send_message(f"/{sf} {jisan}")
+            response = await response
+        except YouBlockedUserError:
+            await cat.edit("```Unblock @themememakerbot plox```")
+            return
+        else:
+            await cat.delete()
+            await event.client.send_message(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+@bot.on(admin_cmd(pattern="talkme ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    jisan = event.pattern_match.group(1)
+    ttm = f"ttm"
+    cat = await event.edit("```Wait making your hardcore meme...```")
+    async with event.client.conversation("@themememakerbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=740813545)
+            )
+            await conv.send_message(f"/{ttm} {jisan}")
+            response = await response
+        except YouBlockedUserError:
+            await cat.edit("```Unblock @themememakerbot plox```")
+            return
+        else:
+            await cat.delete()
+            await event.client.send_message(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+@bot.on(admin_cmd(pattern="brnsay ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    jisan = event.pattern_match.group(1)
+    bbs = f"bbs"
+    cat = await event.edit("```You can't stop your brain...```")
+    async with event.client.conversation("@themememakerbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=740813545)
+            )
+            await conv.send_message(f"/{bbs} {jisan}")
+            response = await response
+        except YouBlockedUserError:
+            await cat.edit("```Unblock @themememakerbot plox```")
+            return
+        else:
+            await cat.delete()
+            await event.client.send_message(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+@bot.on(admin_cmd(pattern="sbob ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    jisan = event.pattern_match.group(1)
+    sp = f"sp"
+    cat = await event.edit("```Yaah wait for spongebob...```")
+    async with event.client.conversation("@themememakerbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=740813545)
+            )
+            await conv.send_message(f"/{sp} {jisan}")
+            response = await response
+        except YouBlockedUserError:
+            await cat.edit("```Unblock @themememakerbot plox```")
+            return
+        else:
+            await cat.delete()
+            await event.client.send_message(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+@bot.on(admin_cmd(pattern="child ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    jisan = event.pattern_match.group(1)
+    love = f"love"
+    cat = await event.edit("```Wait for your son...```")
+    async with bot.conversation("@themememakerbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=740813545)
+            )
+            await conv.send_message(f"/{love} {jisan}")
+            response = await response
+        except YouBlockedUserError:
+            await cat.edit("```Unblock @themememakerbot plox```")
+            return
+        else:
+            await cat.delete()
+            await event.client.send_message(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+CMD_HELP.update(
+    {
+        "troll": "__**PLUGIN NAME :** Troll__\
+\n\n📌** CMD ➥** `.fox` <your text>\
+\n**USAGE   ➥  **Send sneeky fox troll \
+\n\n📌** CMD ➥** `.talkme` <your text>\
+\n**USAGE   ➥  **Send you a hardcore meme.\
+\n\n📌** CMD ➥** `.brnsay` <your text>\
+\n**USAGE   ➥  **Send you a sleeping brain meme.\
+\n\n📌** CMD ➥** `.sbob` <your text>\
+\n**USAGE   ➥  **Send you spongebob meme.\
+\n\n📌** CMD ➥** `.child` <your text>\
+\n**USAGE   ➥  **Send you child in trash meme."
+    }
+)
