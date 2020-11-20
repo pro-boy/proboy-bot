30607cc986959118203ed85b677f8e448b91566f
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-21 02:35:50 +0530

    Update nsfw.py

--- userbot/plugins/nsfw.py
+++ userbot/plugins/nsfw.py
@@ -1,6 +1,8 @@
 # Ported from userge by @buddhhu
 # Ported to DC from plus userbot by @hellboi_atul
 
+
+from telethon.tl.types import MessageMediaPhoto
 import os, urllib, requests, asyncio
 from userbot.utils import admin_cmd
 
@@ -33,3 +35,74 @@ async def butts(event):
     os.remove(pic_loc)
     await event.delete()
     await a.delete()
+
+
+
+
+@borg.on(friday_on_cmd(pattern=r"nsfw"))
+async def nsfw(event):
+    url = "https://nsfw-categorize.it/api/upload"
+    await event.edit("`Processing..`")
+    replymsg = await event.get_reply_message()
+    photo = None
+    if replymsg and replymsg.media:
+        if isinstance(replymsg.media, MessageMediaPhoto):
+            photo = await borg.download_media(message=replymsg.photo)
+        elif "image" in replymsg.media.document.mime_type.split("/"):
+            photo = await borg.download_file(replymsg.media.document)
+        else:
+            await event.edit("`Reply To Image.`")
+    if photo:
+        files = {"image": (f"{photo}", open(f"{photo}", "rb"))}
+        r = requests.post(url, files=files).json()
+        if r["status"] == "OK":
+            await event.edit(
+                "This image is classified as " + str(r["data"]["classification"])
+            )
+        else:
+            await event.edit("Response UnsucessFull. Try Again.")
+
+@bot.on(admin_cmd(pattern="detect$", outgoing=True))
+async def detect(event):
+    if Config.DEEP_AI is None:
+        return await edit_delete(
+            event, "Add VAR `DEEP_AI` get Api Key from https://deepai.org/", 5
+        )
+    reply = await event.get_reply_message()
+    if not reply:
+        return await event.edit_delete(
+           "`Reply to any image or non animated sticker !`", 5
+        )
+    catevent = await event.edit("`Downloading the file to check...`")
+    media = await event.client.download_media(reply)
+    if not media.endswith(("png", "jpg", "webp")):
+        return await edit_delete(
+            event, "`Reply to any image or non animated sticker !`", 5
+        )
+    catevent = await event.edit("`Detecting NSFW limit...`")
+    r = requests.post(
+        "https://api.deepai.org/api/nsfw-detector",
+        files={
+            "image": open(media, "rb"),
+        },
+        headers={"api-key": Config.DEEP_AI},
+    )
+    os.remove(media)
+    if "status" in r.json():
+        return await catevent.edit_delete( r.json()["status"])
+    r_json = r.json()["output"]
+    pic_id = r.json()["id"]
+    percentage = r_json["nsfw_score"] * 100
+    detections = r_json["detections"]
+    link = f"https://api.deepai.org/job-view-file/{pic_id}/inputs/image.jpg"
+    result = f"<b>Detected Nudity :</b>\n<a href='{link}'>>>></a> <code>{percentage:.3f}%</code>\n\n"
+    if detections:
+        for parts in detections:
+            name = parts["name"]
+            confidence = int(float(parts["confidence"]) * 100)
+            result += f"<b>• {name}:</b>\n   <code>{confidence} %</code>\n"
+    await catevent.edit(
+        result,
+        link_preview=False,
+        parse_mode="HTML",
+    )
