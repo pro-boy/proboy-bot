593c4c64e40edb6c583d31d453fdc70bfc189913
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 00:22:48 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -119,37 +119,18 @@ def paginate_help(page_number, loaded_plugins, prefix):
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
-    modules = [
-        custom.Button.inline(
-            "{} {} {}".format(
-                Config.EMOJI_TO_DISPLAY_IN_HELP, x, Config.EMOJI_TO_DISPLAY_IN_HELP
-            ),
-            data="us_plugin_{}".format(x),
-        )
-        for x in helpable_plugins
-    ]
-    if number_of_cols == 1:
-        pairs = list(zip(modules[::number_of_cols]))
-    elif number_of_cols == 2:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    else:
-        pairs = list(
-            zip(
-                modules[::number_of_cols],
-                modules[1::number_of_cols],
-                modules[2::number_of_cols],)
-            )
-        if len(modules) % number_of_cols == 1:
+    modules = [custom.Button.inline(
+        "{} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
+        data="us_plugin_{}".format(x))
+        for x in helpable_plugins]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
-       elif len(modules) % number_of_cols == 2:
-        pairs.append((modules[-2], modules[-1]))
-    max_num_pages = ceil(len(pairs) / number_of_rows)    
+    max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[
-            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
-        ] + [
-            (
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
+            [
                 custom.Button.inline(
                     "⌫", data="{}_prev({})".format(prefix, modulo_page)
                 ),
