2385d46d47bb24571d15b281690fcb9cebf8b1a3
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 00:28:49 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -101,7 +101,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = help_string
         reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-            ©DARK COBRA Userbot".format(plugin_name)
+            ©Userbot".format(plugin_name)
         try:
             #fci = [[Button.inline('Go back', 'back')]] 
             fci = custom.Button.inline("Go Back To Main Menu", data="back")
@@ -131,13 +131,8 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-                custom.Button.inline(
-                    "⌫", data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                custom.Button.inline("❎", data="close"),
-                custom.Button.inline(
-                    "⌦", data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
-           ]
+            (custom.Button.inline("⌫", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("❎", data="close"),
+             custom.Button.inline("⌦", data="{}_next({})".format(prefix, modulo_page)))
+        ]
     return pairs
