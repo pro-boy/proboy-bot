17943e3dbe6dccc5165ec0232b2089b057df0581
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 00:48:00 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,80 +1,66 @@
-import asyncio
-
-import requests
-from telethon import functions
-from userbot.helpers import yaml_format
+from userbot import CMD_LIST
+from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
-from userbot import ALIVE_NAME, CMD_HELP, CMD_LIST
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Noob"
-
+from platform import uname
+import sys
+from telethon import events, functions, __version__
 HELPTYPE = Config.HELP_INLINETYPE or True
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "noob"
 
-
-@borg.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
+@command(pattern="^.help ?(.*)")
+#@borg.on(admin_cmd(pattern=r"help ?(.*)"))
 async def cmd_list(event):
-    global HELPTYPE
-    reply_to_id = None
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    input_str = event.pattern_match.group(1)
-    if input_str == "text":
-        string = (
-            "Total {count} commands found in {plugincount} plugins of catuserbot\n\n"
-        )
-        catcount = 0
-        plugincount = 0
-        for i in sorted(CMD_LIST):
-            plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
-            for iter_list in CMD_LIST[i]:
-                string += "    " + str(iter_list)
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!", "-", "_"):
+        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_LIST:
+                string += "⚡️" + i + "\n"
+                for iter_list in CMD_LIST[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
                 string += "\n"
-                catcount += 1
-            string += "\n"
-        if len(string) > 4095:
-            data = string.format(count=catcount, plugincount=plugincount)
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": data}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            url = f"https://nekobin.com/{key}"
-            reply_text = f"**All commands of the catuserbot can be seen [here]({url})**"
-            await event.edit(reply_text)
-            return
-        await event.edit(string.format(count=catcount, plugincount=plugincount))
-        return
-    if input_str:
-        if input_str in CMD_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            catcount = 0
-            for i in CMD_LIST[input_str]:
-                string += f"  •  <code>{i}</code>"
-                string += "\n"
-                catcount += 1
-            await event.edit(
-                string.format(count=catcount, input_str=input_str), parse_mode="HTML"
-            )
-        else:
-            await event.edit(input_str + " is not a valid plugin!")
-            await asyncio.sleep(3)
+            if len(string) > 4095:
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await bot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS** In DARK COBRA",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
+            else:
+                await event.edit(string)
+        elif input_str:
+            if input_str in CMD_LIST:
+                string = "Commands found in {}:".format(input_str)
+                for i in CMD_LIST[input_str]:
+                    string += "    " + i
+                    string += "\n"
+                await event.edit(string)
+            else:
+                await event.edit(input_str + " is not a valid plugin!")
+                await asyncio.sleep(3)
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
-                          \nUserbot Helper to reveal all the plugin names"
-                          
-            tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+            help_string = f"""Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n
+`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername, help_string
+                tgbotusername,
+                help_string
             )
-            await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+            await results[0].click(
+                event.chat_id,
+                reply_to=event.reply_to_msg_id,
+                hide_via=True
+            )
             await event.delete()
-        else:
+         else:
             string = "<b>Please specify which plugin do you want help for !!\
                 \nNumber of plugins : </b><code>{count}</code>\
                 \n<b>Usage:</b> <code>.help</code> plugin name\n\n"
@@ -85,11 +71,46 @@ async def cmd_list(event):
                 catcount += 1
             await event.edit(string.format(count=catcount), parse_mode="HTML")
 
+@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.edit(result.stringify())
 
+
+@borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
+    result = result.stringify()
+    logger.info(result)  # pylint:disable=E0602
+    await event.edit("Telethon UserBot powered byDark_cobra")
+
+
+@borg.on(admin_cmd(pattern="syntax (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+
+    if plugin_name in CMD_LIST:
+        help_string = CMD_LIST[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin."
+        
+        if help_string:
+            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
+        else:
+            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
+    else:
+
+        plugin_syntax = "Enter valid **Plugin** name.\nDo `.check` or `.help` to get list of valid plugin names."
+
+    await event.edit(plugin_syntax)
 
 
 @borg.on(admin_cmd(outgoing=True, pattern="check ?(.*)"))
-
 async def info(event):
     """ For .info command,"""
     args = event.pattern_match.group(1).lower()
@@ -109,25 +130,8 @@ async def info(event):
             string += "• " + f"<code>{str(i)}</code>"
             string += "   "
             catcount += 1
-       
+
             await event.edit(string.format(count=catcount), parse_mode="HTML")
-
-
-@borg.on(admin_cmd(pattern="dc$"))
-async def _(event):
-    result = await bot(functions.help.GetNearestDcRequest())
-    result = (
-        yaml_format(result)
-        + "\n\n**List Of Telegram Data Centres:**\
-                \nDC1 : Miami FL, USA\
-                \nDC2 : Amsterdam, NL\
-                \nDC3 : Miami FL, USA\
-                \nDC4 : Amsterdam, NL\
-                \nDC5 : Singapore, SG\
-                "
-    )
-    await event.edit(result)
-
 
 @borg.on(admin_cmd(outgoing=True, pattern="setinline (true|false)"))
 async def _(event):
@@ -149,3 +153,4 @@ async def _(event):
             await event.edit("`inline mode is enabled`")
         else:
             await event.edit("`inline mode is already disabled`")
+            
