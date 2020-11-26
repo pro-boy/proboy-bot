a1ca7f782486e5e8bc8e19782a0d75fa4f21eadf
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-27 00:53:59 +0530

    Update addcolor.py

--- userbot/plugins/addcolor.py
+++ userbot/plugins/addcolor.py
@@ -1,14 +1,17 @@
-#By @Danish_00
-#Fixed By a NOOB
+# By @Danish_00
+# Fixed By a NOOB
+# Made for DARK COBRA by team Cobra..
+# kang with credits do not edit these lines..
+
 from telethon.tl.types import MessageMediaPhoto
 import os, urllib, requests, asyncio
 from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 
-@borg.on(admin_cmd(pattern="addc$", outgoing=True))
-async def detect(event):
-    if Config.DEEP_AI is None:
-        return await event.edit(
-             "Add VAR DEEP_AI get Api Key from https://deepai.org/")
+DARKCOBRA = Config.DEEP_AI if Config.DEEP_AI else "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+@borg.on(admin_cmd(pattern="colp$", outgoing=True))
+async def _(event):
+    
                
     reply = await event.get_reply_message()
     if not reply:#By @Danish_00
@@ -16,20 +19,20 @@ async def detect(event):
         return await event.edit(
            "Reply to any image or non animated sticker !"
         )
-    catevent = await event.edit("Downloading the file to check...")
+    devent = await event.edit("`Downloading the file😅😁😁....`")
     media = await event.client.download_media(reply)
     if not media.endswith(("png", "jpg", "webp")):
         return await event.edit(
              "Reply to any image or non animated sticker !"
         )#By @Danish_00
 #Fixed By a NOOB
-    devent = await event.edit("coloring image sar...")
+    devent = await event.edit("`Coloring image 🎨🖌️...`")#hehehhehehhe
     r = requests.post(
         "https://api.deepai.org/api/colorizer",
         files={
             "image": open(media, "rb"),
         },
-        headers={"api-key": Config.DEEP_AI},
+        headers={"api-key": DARKCOBRA},
     )#By @Danish_00
 #Fixed By a NOOB
     os.remove(media)
@@ -42,7 +45,16 @@ async def detect(event):
     result = f"{r_json}"
     
     await devent.delete()
-    await borg.send_message(
+    await borg.send_message(#hehehhehehehehheh
         event.chat_id,
         file=result
     )
+
+
+#hehehehehe
+CMD_HELP.update(
+    {
+        "color": 
+    ".colp <reply to any black nd white media> "
+    "\nIt colorize any black-and-white pic/sticker   `(Note :-if its not working then go to deepai.org then get api and set var DEEP_AI nd key.)`"
+    })
