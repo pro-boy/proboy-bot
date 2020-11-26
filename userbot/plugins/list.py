5da14e220e25855b2b20d723918dfdb39c4da0eb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-27 00:58:59 +0530

    Update list.py

--- userbot/plugins/list.py
+++ userbot/plugins/list.py
@@ -1,126 +1,50 @@
-# Copyright (C) 2020 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
 """
-import io
+List Files plugin for userbot //Simple Module for people who dont wanna use shell executor for listing files.
+cmd: .ls // will return files from current working directory
+	 .ls path // will return output according to path  
+
+By:- @Zero_cool7870
+
+"""
+
+from uniborg.util import admin_cmd
+import asyncio
 import os
-import os.path
-import time
-from os.path import exists, isdir
-
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd, humanbytes
-
-MAX_MESSAGE_SIZE_LIMIT = 4095
 
 
-@bot.on(admin_cmd(outgoing=True, pattern=r"ls ?(.*)"))
+@borg.on(admin_cmd(pattern="ls ?(.*)"))
 async def lst(event):
-    if event.fwd_from:
-        return
-    tele = event.pattern_match.group(1)
-    if tele:
-        path = tele
-    else:
-        path = os.getcwd()
-    if not exists(path):
-       await event.edit(
-            f"There is no such directory or file with the name `{tele}` check again!",
-        )
-        return
-    if isdir(path):
-        if tele:
-            msg = "Folders and Files in `{}` :\n\n".format(path)
-            lists = os.listdir(path)
-        else:
-            msg = "Folders and Files in Current Directory :\n\n"
-            lists = os.listdir(path)
-        files = ""
-        folders = ""
-        for contents in sorted(lists):
-            telepath = path + "/" + contents
-            if not isdir(telepath):
-                size = os.stat(telepath).st_size
-                if contents.endswith((".mp3", ".flac", ".wav", ".m4a")):
-                    files += "🎵 " + f"`{contents}`\n"
-                if contents.endswith((".opus")):
-                    files += "🎙 " + f"`{contents}`\n"
-                elif contents.endswith(
-                    (".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")
-                ):
-                    files += "🎞 " + f"`{contents}`\n"
-                elif contents.endswith(
-                    (".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")
-                ):
-                    files += "🗜 " + f"`{contents}`\n"
-                elif contents.endswith(
-                    (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ". webp")
-                ):
-                    files += "🖼 " + f"`{contents}`\n"
-                elif contents.endswith((".exe", ".deb")):
-                    files += "⚙️ " + f"`{contents}`\n"
-                elif contents.endswith((".iso", ".img")):
-                    files += "💿 " + f"`{contents}`\n"
-                elif contents.endswith((".apk", ".xapk")):
-                    files += "📱 " + f"`{contents}`\n"
-                elif contents.endswith((".py")):
-                    files += "🐍 " + f"`{contents}`\n"
-                else:
-                    files += "📄 " + f"`{contents}`\n"
-            else:
-                folders += f"📁 `{contents}`\n"
-        if files or folders:
-            msg = msg + folders + files
-        else:
-            msg = msg + "__empty path__"
-    else:
-        size = os.stat(path).st_size
-        msg = "The details of given file :\n\n"
-        if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
-            mode = "🎵 "
-        if path.endswith((".opus")):
-            mode = "🎙 "
-        elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
-            mode = "🎞 "
-        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
-            mode = "🗜 "
-        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ". webp")):
-            mode = "🖼 "
-        elif path.endswith((".exe", ".deb")):
-            mode = "⚙️ "
-        elif path.endswith((".iso", ".img")):
-            mode = "💿 "
-        elif path.endswith((".apk", ".xapk")):
-            mode = "📱 "
-        elif path.endswith((".py")):
-            mode = "🐍 "
-        else:
-            mode = "📄 "
-        time.ctime(os.path.getctime(path))
-        time2 = time.ctime(os.path.getmtime(path))
-        time3 = time.ctime(os.path.getatime(path))
-        msg += f"**Location :** `{path}`\n"
-        msg += f"**Icon :** `{mode}`\n"
-        msg += f"**Size :** `{humanbytes(size)}`\n"
-        msg += f"**Last Modified Time:** `{time2}`\n"
-        msg += f"**Last Accessed Time:** `{time3}`"
+	if event.fwd_from:
+		return
+	input_str = event.pattern_match.group(1)
+	if input_str:
+		msg = "**Files in {} :**\n".format(input_str)
+		files = os.listdir(input_str)
+	else:
+		msg = "**Files in Current Directory :**\n"
+		files = os.listdir(os.getcwd())
+	for file in files:
+		msg += "`{}`\n".format(file)
+	if len(msg) <= Config.MAX_MESSAGE_SIZE_LIMIT:
+		await event.edit(msg)
+	else:
+		msg = msg.replace("`","")
+		out = 'filesList.txt'
+		with open(out,'w') as f:
+			f.write(f)
+		await borg.send_file(
+				event.chat_id,
+				out,
+				force_document=True,
+				allow_cache=False,
+				caption="`Output is huge. Sending as a file...`"
+		)
+		await event.delete()	
 
-    if len(msg) > MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(msg)) as out_file:
-            out_file.name = "ls.txt"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=path,
-            )
-            await event.delete()
-    else:
-        await event.edit(msg)
+			
 
+			
 
-CMD_HELP.update({"file": ".ls <directory>" "\nUsage: File Manager plugin for TeleBot."})
-"""
+				
+
+        
